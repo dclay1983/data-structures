6426836ed7c83f99d4ae5202ffda283549ac59c1
Philip Enchin <Philip@Ench.in>
2019-10-23 17:34:38 -0700

    Implement linkedList

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,28 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
+    var node = Node(value);
+    if (this.head === null) {
+      this.head = this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
   };
 
   list.removeHead = function() {
+    var node = this.head;
+    this.head = node.next;
+    return node.value;
   };
 
   list.contains = function(target) {
+    var node = this.head;
+    while (node) {
+      if (node.value === target) { return true; }
+      node = node.next;
+    }
+    return false;
   };
 
   return list;
