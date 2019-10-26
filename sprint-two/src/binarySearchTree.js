d42cfa88dfd985e68b10853e54f47d20765b85aa
Philip Enchin <Philip@Ench.in>
2019-10-25 19:22:00 -0700

    Implement binary search tree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,42 @@
-var BinarySearchTree = function(value) {
-};
+class BinarySearchTree {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(value) {
+    let key = '';
+    if (this.value === value) { return; }
+    key = value < this.value ? 'left' : 'right';
+
+    if (this[key]) {
+      this[key].insert(value);
+    } else {
+      this[key] = new BinarySearchTree(value);
+    }
+  }
+
+  contains(value) {
+    if (value === this.value) {
+      return true;
+    }
+    var key = value < this.value ? 'left' : 'right';
+    return this[key] ? this[key].contains(value) : false;
+  }
+
+  depthFirstLog(cb) {
+    cb(this.value);
+    if (this.left) { this.left.depthFirstLog(cb); }
+    if (this.right) { this.right.depthFirstLog(cb); }
+  }
+}
 
 
 /*
  * Complexity: What is the time complexity of the above functions?
+ * Creation O(1) Constant
+ * insert O(log(n)) Logarithmic
+ * contains O(log(n)) Logarithmic
+ * depthFirstLog O(n) Linear
  */
